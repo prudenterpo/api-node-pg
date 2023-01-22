3bc6eb550e9cec58f614a76615f638696b10b737
rodrigopdo <rp@meuagroforte.com.br>
2023-01-22 19:10:58 -0300

    feat: Initial code implemented

---
+++ src/student/controller.js
@@ -0,0 +1,16 @@
+const pool = require('../../db');
+
+const getStudents = (req, res) => {
+  pool.query("SELECT * FROM students", (error, results) => {
+    if(error) throw error;
+    res.status(200).json(results.rows);
+  })
+};
+
+module.exports = {
+  getStudents,
+}
+
+
+
+
